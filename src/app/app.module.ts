303fd417f9523e03c91451639ca9c2f0ffc9475c
pjooklas <pjooklas@users.noreply.github.com>
2022-07-05 19:22:24 +0300

    footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { CountriesComponent } from './components/countries/countries.component';
 import { FormsModule } from '@angular/forms';
 import { NgxPaginationModule } from 'ngx-pagination';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CountriesComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
